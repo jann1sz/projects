2fee03069adb7752ec40fe0d962febabfd777547
jannis <62758393+jann1sz@users.noreply.github.com>
2021-11-05 12:49:19 -0400

    Update and rename extract min AND pop sorted.py to extract min, pop sorted, pop sort.py

--- school projects/python/extract min, pop sorted, pop sort.py
+++ school projects/python/extract min, pop sorted, pop sort.py
@@ -37,7 +37,8 @@ extract_min([9,9,3,8,8,3,7,20,6,3])
 #-------------------------------------------------------------------------------
 
 #this function uses the extract min function to find the minimum and arranges
-#xs in order from smallest to largest values.
+#xs in order from smallest to largest values, except we don't actually modify
+#xs, we return another list.
 def pop_sorted(xs):
 
     #copy list holds the copy of xs
@@ -65,3 +66,35 @@ def pop_sorted(xs):
     return ordered
 
 pop_sorted([2,3,4,2,4,6,5])
+
+#-----------------------------------------------------------------------------
+#for this function, it is extremely similar to the function above, but this
+#time we actually modify xs, the given list parameter.
+def pop_sort(xs):
+    
+    #copy list holds the copy of xs
+    #order_t list is temporary and holds the ordered list of xs
+    copy = []
+    order_t = []
+    
+    #loop that appends all values in xs to the list copy
+    for i in range(len(xs)):
+        value = xs[i]
+        copy.append(value)
+    
+    #loop that uses extract_min to find the minimum and append it to the
+    #ordered list
+    for i in range(len(copy)):
+        minimum = extract_min(copy)
+        order_t.append(minimum)
+    
+    #loop that alters xs by replacing a value xs[a] with the number in the
+    #ordered list that corresponds with the same index of a
+    for a in range(len(xs)):
+        value = order_t[a]
+        
+        xs[a] = value
+    
+    return None
+
+pop_sort([5,1,6,2,7,3,8,4,9,0])
